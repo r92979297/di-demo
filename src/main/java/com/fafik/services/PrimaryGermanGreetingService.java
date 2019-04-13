20d985488bd34447b9b92b4026b548ed9b7178e7
fafik <r92979297@gmail>
2019-04-13 13:00:58 +0200

    di-demo refactor to config service with beans

--- src/main/java/com/fafik/services/PrimaryGermanGreetingService.java
+++ src/main/java/com/fafik/services/PrimaryGermanGreetingService.java
@@ -1,12 +1,5 @@
 package com.fafik.services;
 
-import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Service;
-
-@Service
-@Primary
-@Profile("de")
 public class PrimaryGermanGreetingService implements GreetingService {
     private GreetingRepository greetingRepository;
 
